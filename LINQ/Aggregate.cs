7c1d8c39eaa0be64649884d307f7c774eb2d2cf1
Frai <fraifelipe@gmail.com>
2019-10-25 22:47:48 -0300

    Aggregate

---
+++ LINQ/Aggregate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace CSharpReference.LINQ
+{
+    public class Aggregate: ICode
+    {
+        public int Code => 5;
+        public void Execute()
+        {
+            var result = new[]{1,2,3,4,5}.Aggregate((a,b) => a + b);
+            
+            Console.WriteLine(result);
+        }
+    }
+}

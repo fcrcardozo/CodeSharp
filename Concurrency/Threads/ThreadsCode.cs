31fb23020d2915d3a5db837ca1cba0516f98fbc2
Felipe Cardozo <fcrcardozo@outlook.com>
2019-12-25 20:01:37 -0300

    Concurrency

---
+++ Concurrency/Threads/ThreadsCode.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading;
+
+namespace CSharpReference.Concurrency.Threads
+{
+    public class ThreadsCode : ICode
+    {
+        public async void Execute()
+        {
+            //Using await has order
+
+            //Run first
+            var bigCode = new Action(() => GetSha256(new byte[10000000]));
+            var firstThreadStarted = new ThreadStart(bigCode);
+            new Thread(firstThreadStarted).Start();
+
+
+            //Run second
+            var smallCode = new Action(() => GetSha256(new byte[100]));
+            var secondThreadStarted = new ThreadStart(smallCode);
+            new Thread(secondThreadStarted).Start();
+        }
+
+        private static void GetSha256(byte[] data)
+        {
+            var sha256 = new SHA256Managed();
+            var retval = sha256.ComputeHash(data);
+            var hash = retval.Aggregate(string.Empty, (current, theByte) => current + theByte.ToString("x2"));
+
+            Console.WriteLine(hash);
+            Console.WriteLine(data.Length);
+        }
+    }
+}

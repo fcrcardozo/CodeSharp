be5fa79dff198cb2c15417a0b2467198bd9efa70
Felipe Cardozo <fraifelipe@gmail.com>
2021-02-17 23:41:46 -0300

    MedianOfTwoSortedArrays

---
+++ CodeSharp.Tests/LeetCode/0004/MedianOfTwoSortedArraysTests.cs
@@ -0,0 +1,22 @@
+﻿using CSharpReference.LeetCode._0004;
+using FluentAssertions;
+using Xunit;
+
+namespace CodeSharp.Tests.LeetCode._0004
+{
+    public class MedianOfTwoSortedArraysTests
+    {
+        [Theory]
+        [InlineData(new[]{1,3}, new[]{2}, 2.0)]
+        [InlineData(new[]{1,2}, new[]{3, 4}, 2.5)]
+        [InlineData(new[]{1,2}, new[]{1, 2}, 1.5)]
+        public void MustFindMedianSortedArrays(int[] nums1, int[] nums2, double expected)
+        {
+            var sut = new MedianOfTwoSortedArrays();
+            
+            var result = sut.FindMedianSortedArrays(nums1, nums2);
+
+            result.Should().Be(expected);
+        } 
+    }
+}
---
+++ CodeSharp/LeetCode/0004/MedianOfTwoSortedArrays.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace CSharpReference.LeetCode._0004
+{
+    public class MedianOfTwoSortedArrays
+    {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            var result = nums1.Union(nums2).OrderBy(x => x).ToArray();
+            var index = (int) Math.Floor(result.Count() / 2.0) - 1;
+            
+
+            if (result.Count() % 2 == 1)
+            {
+                return result[index + 1];
+            }
+
+            var a = result[index];
+            var b = result[index + 1];
+
+            return (a + b) / 2.0;
+        } 
+    }
+}
